57df6e165e27f3b3cc7f9de21e18a12e12035a1e
gabriellamiwa <gabriellakaku@gmail.com>
2023-11-17 21:55:08 -0500

    Update app_config.py

--- app_config.py
+++ app_config.py
@@ -1,8 +1,15 @@
+CONST_MONGO_URL = "mongodb+srv://gabriellakaku:uU16RA3hLl1hgOnX@cluster0.glpwbre.mongodb.net/?retryWrites=true&w=majority"
+CONST_DATABASE_NAME= "TEST"
+CONST_USER_COLLECTION = "users"
+CONST_VERB_COLLECTION = "verbs"
+
+
+'''
 CONST_MONGO_URL = "mongodb+srv://landeirob:DQIeF2UPMRkexpze@cluster0.rnfjvlw.mongodb.net/?retryWrites=true&w=majority"
 CONST_DATABASE_NAME= "test"
 CONST_USER_COLLECTION = "users"
 
-'''
+
 CONST_MONGO_URL = "mongodb+srv://renancavalcanti:PQ7TJXmsdxosVqjP@cluster0.i9ldur9.mongodb.net/?retryWrites=true&w=majority"
 CONST_DATABASE_NAME = "TEST"
 CONST_USER_COLLECTION = "users"
