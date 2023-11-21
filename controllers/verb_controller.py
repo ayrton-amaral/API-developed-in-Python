321f30b62be75ca3dba7a6aab70a70a9040faf8b
Bruno Landeiro <landeiro.b@gmail.com>
2023-11-21 10:51:36 -0500

    Criacao de helper para a api externa
    Resolvi criar esse arquivo só pra ficar um pouco mais organizado. Mas antes já estava ótimo.

--- controllers/verb_controller.py
+++ controllers/verb_controller.py
@@ -2,16 +2,14 @@ from models.verb_model import Verb
 from database.__init__ import database
 import app_config as config
 from flask import jsonify
-import requests
 from bson.objectid import ObjectId
+from helpers.external_api import getVerbFromApi
 
 def get_verb(userInput):
     try:
         verb = userInput["verb"]
-        external_api_url = 'https://lasalle-frenchverb-api-afpnl.ondigitalocean.app/v1/api/verb'
-        response = requests.get(external_api_url, headers={'token':
-        '278ef2169b144e879aec4f48383dce28e654a009cacf46f8b6c03bbc9a4b9d11'}, json={'verb':
-        verb})
+
+        response = getVerbFromApi(verb)
 
         if response.status_code == 200:
             return jsonify({"verb": response.json()})
@@ -25,8 +23,7 @@ def favorite_verb(userInput, tokenUser):
         verb = userInput["verb"]
         user_id = tokenUser.get('uid', None)
 
-        external_api_url = 'https://lasalle-frenchverb-api-afpnl.ondigitalocean.app/v1/api/verb'
-        response = requests.get(external_api_url, headers={'token': '278ef2169b144e879aec4f48383dce28e654a009cacf46f8b6c03bbc9a4b9d11'}, json={'verb': verb})
+        response = getVerbFromApi(verb)
 
         if response.status_code == 200:
             collection = database.dataBase[config.CONST_USER_COLLECTION]
